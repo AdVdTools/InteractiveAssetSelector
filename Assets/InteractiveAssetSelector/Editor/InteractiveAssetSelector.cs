dd2354f0f2a681ae079ab94d118befb5dc665e9a
AdVdTools <ngemonji@gmail.com>
2016-10-19 00:16:13 +0200

    Enhanced asset listing
    Also OnEndGUI delegate for custom purposes

--- Assets/InteractiveAssetSelector/Editor/InteractiveAssetSelector.cs
+++ Assets/InteractiveAssetSelector/Editor/InteractiveAssetSelector.cs
@@ -9,12 +9,14 @@ public class InteractiveAssetSelector : EditorWindow {
 	class SelectionAsset : IComparer<SelectionAsset> {
 		public Object asset;
 		public string path;
-		public bool selected;
+		public bool selected;//if folder this will work as foldout
+		public bool isFolder;
 
-		public SelectionAsset(Object asset, bool selected = false) {
+		public SelectionAsset(Object asset, bool selected = true) {
 			this.asset = asset;
 			this.path = AssetDatabase.GetAssetPath(asset);
 			this.selected = selected;
+			this.isFolder = Directory.Exists(this.path);
 		}
 		//TODO how did I implement the comparer for list sort?
 
@@ -31,41 +33,61 @@ public class InteractiveAssetSelector : EditorWindow {
 	private List<SelectionAsset> selection = new List<SelectionAsset>();
 	private List<string> visiblePaths = new List<string>();
 
+	public System.Action<InteractiveAssetSelector> OnEndGUI;
+
 	[MenuItem("Assets/Interactive Asset Selector")]
-	public static void InitSelector() {
-		InitSelector(Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets));
+	public static InteractiveAssetSelector InitSelector() {
+		return InitSelector(Selection.GetFiltered(typeof(Object), SelectionMode.DeepAssets));
 	}
 
-	public static void InitSelector(Object[] selection) {
+	[MenuItem("Assets/Custom Exporter")]
+	public static InteractiveAssetSelector Export() {
+		InteractiveAssetSelector ias = InitSelector();
+		ias.OnEndGUI = (selector) => {
+			EditorGUILayout.Space();
+			if (GUILayout.Button("Export")){
+				Debug.Log("Export");
+			}
+		};
+		return ias;
+	}
+
+	public static InteractiveAssetSelector InitSelector(Object[] selection) {
 		InteractiveAssetSelector ias = GetWindow<InteractiveAssetSelector>("Select Assets");
 
 		ias.selection.Clear();
 		ias.selection.Capacity = selection.Length;
 		foreach(Object obj in selection) {
-			//TODO sorted insert
-			string path = AssetDatabase.GetAssetPath(obj);
-			Debug.Log(path+" "+AssetDatabase.AssetPathToGUID(path));
-			if (path.Equals("") || Directory.Exists(path)) {//Not an asset file
-				continue;
-			}
 			ias.SortedInsert(obj);
 		}
 //		ias.selection.Sort((x,y) => EditorUtility.NaturalCompare(x.path, y.path));
 
 		ias.Show();
+		return ias;
 	}
 
 	public void SortedInsert(Object obj) {
 		SortedInsert(new SelectionAsset(obj));
 	}
 
+	public void SortedInsert(string path) {
+		SortedInsert(new SelectionAsset(AssetDatabase.LoadAssetAtPath(path, typeof(Object))));
+	}
+
 	void SortedInsert(SelectionAsset asset) {
+		//check parent folders first
+		int parentPathLength = asset.path.LastIndexOf('/');
+		if (parentPathLength >= 0) {
+			SortedInsert(asset.path.Substring(0, parentPathLength));//Try to add parent
+		}
+
 		int index = selection.BinarySearch(asset, asset);//TODO there must be another way
 		if (index < 0) {
 			selection.Insert(~index, asset);
 		}
 	}
 
+	Vector2 scrollPosition;
 	void OnGUI() {
 		string currentPath = "";
 		List<string> currentSplittedPath = new List<string>();
@@ -73,35 +95,57 @@ public class InteractiveAssetSelector : EditorWindow {
 		int visiblePathIndex = 0;
 
 
-		foreach (SelectionAsset asset in selection) {
-			GUILayout.Label(asset.asset.name+" "+asset.asset.GetType()+" "+asset.path);
-		}
+//		foreach (SelectionAsset asset in selection) {
+//			GUILayout.Label(asset.asset.name+" "+asset.asset.GetType()+" "+asset.path);
+//		}
+//
+//		EditorGUILayout.Space();
 
-		EditorGUILayout.Space();
-
+		EditorGUILayout.BeginScrollView(scrollPosition);
 		foreach (SelectionAsset asset in selection) {
 			string path = asset.path;
 
-			//TODO use split and depth
-			string[] splittedPath = path.Split('/');
+			int parentPathLength = path.LastIndexOf('/');
+			string parent = parentPathLength < 0 ? "" : path.Substring(0, parentPathLength);
+			if (!currentPath.StartsWith(parent)) continue;//Hidden
+			currentPath = parent;
+			EditorGUI.indentLevel = PathDepth(currentPath)+1;
 
-			int matchDepth = 0;
-			while (matchDepth < currentSplittedPath.Count && matchDepth < splittedPath.Length) {
-				if (!currentSplittedPath[matchDepth].Equals(splittedPath[matchDepth])) {
-					break;
+			string name = path.Substring(parentPathLength+1);//TODO make property?
+			if (asset.isFolder) {
+				asset.selected = EditorGUILayout.Foldout(asset.selected, name);
+				if (asset.selected) {
+					currentPath = asset.path;
 				}
-				matchDepth++;
 			}
-			EditorGUI.indentLevel = matchDepth;
-			currentSplittedPath.RemoveRange(matchDepth, currentSplittedPath.Count - matchDepth);
-			while (matchDepth < splittedPath.Length - 1) {
-				string folder = splittedPath[matchDepth++];
-				currentSplittedPath.Add(folder);
-				EditorGUILayout.Foldout(true, folder);
-				EditorGUI.indentLevel++;
+			else {
+				asset.selected = EditorGUILayout.ToggleLeft(name, asset.selected);//
 			}
-			EditorGUI.indentLevel++;//TODO icon content
-			EditorGUILayout.LabelField(splittedPath[matchDepth]);
+
+
+
+			//TODO use split and depth
+//			string[] splittedPath = path.Split('/');
+//
+//			int matchDepth = 0;
+//			while (matchDepth < currentSplittedPath.Count && matchDepth < splittedPath.Length) {
+//				if (!currentSplittedPath[matchDepth].Equals(splittedPath[matchDepth])) {
+//					break;
+//				}
+//				matchDepth++;
+//			}
+//			EditorGUI.indentLevel = matchDepth;
+//			currentSplittedPath.RemoveRange(matchDepth, currentSplittedPath.Count - matchDepth);
+//			while (matchDepth < splittedPath.Length - 1) {
+//				string folder = splittedPath[matchDepth++];
+//				currentSplittedPath.Add(folder);
+//				EditorGUILayout.Foldout(true, folder);
+//				EditorGUI.indentLevel++;
+//			}
+//			EditorGUI.indentLevel++;//TODO icon content
+//			EditorGUILayout.LabelField(splittedPath[matchDepth]);
+
+
 			//
 //			while (!path.StartsWith(currentPath)){
 //				int parentPathLength = currentPath.LastIndexOf('/');
@@ -141,6 +185,7 @@ public class InteractiveAssetSelector : EditorWindow {
 
 		}
 
+		EditorGUILayout.EndScrollView();
 //		foreach(SelectionAsset asset in selection) {
 //			string path = asset.path;
 //			string parentPath = Directory.GetParent(path).ToString();
@@ -149,5 +194,20 @@ public class InteractiveAssetSelector : EditorWindow {
 //				string
 //			}
 //		}
+
+		//TODO drag and drop
+
+		if (OnEndGUI != null) OnEndGUI(this);
+	}
+
+	int PathDepth(string path) {
+		if (path.Equals("")) {
+			return -1;
+		}
+		int count = 0, index = 0;
+		while((index = path.IndexOf('/', index+1)) != -1) {
+			count++;
+		}
+		return count;
 	}
 }
